00806ef3aef3ed27e7e5468b52c7db2e70ee704b
stozuka <stozuka@gmail.com>
2017-05-07 14:11:42 +0800

    Create lib

--- controllers/taipei/index.js
+++ controllers/taipei/index.js
@@ -1,39 +1,21 @@
 'use strict';
 
-const rp = require('request-promise');
 const debug = require('debug')('search-youbike-stations:server');
+const rp = require('request-promise');
+const queryValidator = require('../../lib/query_validator');
 
 function processResults(parsed) {
   return parsed;
-}
-
-function isValidLatLong(query) {
-  const lat = parseFloat(query.lat);
-  const lon = parseFloat(query.lon);
-  // Check if lat and lon are numbers
-  if (!typeof lat === 'number' || !typeof lon === 'number') {
-    return false;
-  }
-  // Check if the range is valid
-  if (Math.abs(lat) > 90 || Math.abs(lon) > 180) {
-    return false;
-  }
-  // If no problem, return true
-  return true;
-}
-
-function isInTaipei(query) {
-  return true;
 }
 
 function getStations(req, res, next) {
   const query = req.query;
 
   // Validation of query values
-  if (!isValidLatLong(query)) {
+  if (!queryValidator.isValidLatLong(query)) {
     return res.status(200).json({code: -1, results: []});
   }
-  if (!isInTaipei(query)) {
+  if (!queryValidator.isInTaipei(query)) {
     return res.status(200).json({code: -2, results: []});
   }
 
@@ -50,7 +32,7 @@ function getStations(req, res, next) {
   // Send GET request and send reponse
   rp.get(options)
     .then(results => processResults(results))
-    .then(processed => res.status(200).json(processed))
+    .then(resObj => res.status(200).json(resObj))
     .catch(_ => res.status(200).json({code: -3, results: []}));
 }
 
