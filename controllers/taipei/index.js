76441d1df016e5720aec05eefa1b3beb7529eda1
stozuka <stozuka@gmail.com>
2017-05-07 13:52:26 +0800

    Add query validation

--- controllers/taipei/index.js
+++ controllers/taipei/index.js
@@ -1,12 +1,24 @@
 'use strict';
 
 const rp = require('request-promise');
+const debug = require('debug')('search-youbike-stations:server');
 
 function processResults(parsed) {
   return parsed;
 }
 
 function isValidLatLong(query) {
+  const lat = parseFloat(query.lat);
+  const lon = parseFloat(query.lon);
+  // Check if lat and lon are numbers
+  if (!typeof lat === 'number' || !typeof lon === 'number') {
+    return false;
+  }
+  // Check if the range is valid
+  if (Math.abs(lat) > 90 || Math.abs(lon) > 180) {
+    return false;
+  }
+  // If no problem, return true
   return true;
 }
 
