b5939f2c2db9e044a8df1a63e73792022c825ac0
stozuka <stozuka@gmail.com>
2017-05-08 20:50:51 +0800

    Return nearest 2 stations

--- controllers/taipei/index.js
+++ controllers/taipei/index.js
@@ -2,21 +2,52 @@
 
 const debug = require('debug')('search-youbike-stations:server');
 const rp = require('request-promise');
+const geolib = require('geolib');
 const queryValidator = require('../../lib/query_validator');
 
-function processResults(parsed) {
-  return parsed;
+function processResults(stations, query) {
+  const {lat, lng} = query;
+  let ret = [];
+
+  stations.forEach(station => {
+    const numUbike = parseInt(station.sbi);
+
+    // Continue if no bike is available
+    if (numUbike === 0) { return; }
+
+    const dist = geolib.getDistance(
+      {latitude: lat, longitude: lng},
+      {latitude: station.lat, longitude: station.lng}
+    );
+    const obj = {station: station.sna, num_ubike: numUbike, dist: dist};
+    ret.push(obj);
+  });
+
+  // If there was no station with available bike
+  if (ret.length === 0) {
+    return {code: 1, result: []};
+  }
+
+  // If there are stations available
+  ret.sort((a, b) => a.dist - b.dist);
+  ret.forEach(elem => delete elem.dist);
+  return {code: 0, result: ret.slice(0, 2)};
 }
 
 function getStations(req, res, next) {
   const query = req.query;
 
+  // Convert datatype of query from string to float
+  for (let key in query) {
+    query[key] = parseFloat(query[key])
+  }
+
   // Validation of query values
   if (!queryValidator.isValidLatLong(query)) {
-    return res.status(200).json({code: -1, results: []});
+    return res.status(200).json({code: -1, result: []});
   }
   if (!queryValidator.isInTaipei(query)) {
-    return res.status(200).json({code: -2, results: []});
+    return res.status(200).json({code: -2, result: []});
   }
 
   // Settings of GET request
@@ -29,11 +60,11 @@ function getStations(req, res, next) {
     json: true
   };
 
-  // Send GET request and send reponse
+  // Send GET request to YouBike API and process results
   rp.get(options)
-    .then(results => processResults(results))
+    .then(response => processResults(response.result.results, query))
     .then(resObj => res.status(200).json(resObj))
-    .catch(_ => res.status(200).json({code: -3, results: []}));
+    .catch(_ => res.status(200).json({code: -3, result: []}));
 }
 
 module.exports = {
